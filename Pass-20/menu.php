60beab4f1ca3b123a44b131da52b26f40f18f6a5
jasmine-95 <1465597473@qq.com>
2025-07-20 13:16:21 +0800

    Add files via upload

---
+++ Pass-20/menu.php
@@ -0,0 +1,5 @@
+<div id="menu">     
+	<ul id="menulist">         
+		<li><a id="Pass-20" href="<?php echo APP_URL_ROOT;?>/Pass-20/index.php">Pass-20</a></li> 
+	</ul> 
+</div>
---
+++ Pass-21/menu.php
@@ -0,0 +1,5 @@
+<div id="menu">     
+	<ul id="menulist">         
+		<li><a id="Pass-21" href="<?php echo APP_URL_ROOT;?>/Pass-21/index.php">Pass-21</a></li> 
+	</ul> 
+</div>
